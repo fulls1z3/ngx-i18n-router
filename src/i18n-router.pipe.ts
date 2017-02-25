af3d2e9a2546d4b1cd67fe6b44f2fc2a7aabc83f
Burak Tasci <me@fulls1z3.com>
2017-02-26 00:36:38 +0300

    fix: depend on Angular 2.0.0 (#3)
    * build(npm): update deps
    
    * build(gulp): update tasks
    
    * build(webpack): update config
    
    * refactor: update .gitignore
    
    * refactor: update .npmignore
    
    * style(core): lint

--- src/i18n-router.pipe.ts
+++ src/i18n-router.pipe.ts
@@ -7,56 +7,57 @@ import { I18NRouterService, ROOT_ROUTE_PREFIX } from './i18n-router.service';
 
 @Injectable()
 @Pipe({
-    name: 'i18nRouter',
-    pure: false // required to update the value when the promise is resolved
+  name: 'i18nRouter',
+  pure: false // required to update the value when the promise is resolved
 })
 export class I18NRouterPipe implements PipeTransform {
-    constructor(private readonly router: Router,
-                private readonly i18nRouter: I18NRouterService) {}
+  constructor(private readonly router: Router,
+              private readonly i18nRouter: I18NRouterService) {
+  }
 
-    transform(query: string | Array<any>): string {
-        if (typeof query === 'string' && !!query && query.length)
-            throw new Error('Query must be an empty string or an array!');
+  transform(query: string | Array<any>): string {
+    if (typeof query === 'string' && !!query && query.length)
+      throw new Error('Query must be an empty string or an array!');
 
-        if (!this.i18nRouter.languageCode || !this.i18nRouter.useLocalizedRoutes)
-            return `/${typeof query === 'string' ? query : query.join('/')}`;
+    if (!this.i18nRouter.languageCode || !this.i18nRouter.useLocalizedRoutes)
+      return `/${typeof query === 'string' ? query : query.join('/')}`;
 
-        if (!query || query.length === 0)
-            return `/${this.i18nRouter.languageCode}`;
+    if (!query || query.length === 0)
+      return `/${this.i18nRouter.languageCode}`;
 
-        return `/${this.translateQuery(query)}`;
-    }
+    return `/${this.translateQuery(query)}`;
+  }
 
-    private translateQuery(key: string | Array<any>): string {
-        const translateBatch: Array<any> = [];
-        let batchKey = '';
+  private translateQuery(key: string | Array<any>): string {
+    const translateBatch: Array<any> = [];
+    let batchKey = '';
 
-        (key as Array<any>).forEach((segment: any, index: number) => {
-            if (typeof segment === 'string') {
-                let prefix = '';
+    (key as Array<any>).forEach((segment: any, index: number) => {
+      if (typeof segment === 'string') {
+        let prefix = '';
 
-                let currentKey = `${ROOT_ROUTE_PREFIX}.${segment}`;
+        let currentKey = `${ROOT_ROUTE_PREFIX}.${segment}`;
 
-                if (index === 0) {
-                    prefix = this.i18nRouter.getTranslation(currentKey);
+        if (index === 0) {
+          prefix = this.i18nRouter.getTranslation(currentKey);
 
-                    if (!!prefix) {
-                        batchKey = currentKey;
-                        translateBatch.push(this.i18nRouter.languageCode);
-                    }
-                }
+          if (!!prefix) {
+            batchKey = currentKey;
+            translateBatch.push(this.i18nRouter.languageCode);
+          }
+        }
 
-                currentKey = index === 0 ? (!!prefix ? batchKey : segment) : `${batchKey}.${segment}`;
-                const translatedSegment = this.i18nRouter.getTranslation(currentKey);
+        currentKey = index === 0 ? (!!prefix ? batchKey : segment) : `${batchKey}.${segment}`;
+        const translatedSegment = this.i18nRouter.getTranslation(currentKey);
 
-                if (!!translatedSegment)
-                    batchKey = currentKey;
+        if (!!translatedSegment)
+          batchKey = currentKey;
 
-                translateBatch.push(translatedSegment || segment);
-            } else
-                translateBatch.push(segment);
-        });
+        translateBatch.push(translatedSegment || segment);
+      } else
+        translateBatch.push(segment);
+    });
 
-        return translateBatch.join('/');
-    }
+    return translateBatch.join('/');
+  }
 }
--- tests/i18n-router.loader.spec.ts
+++ tests/i18n-router.loader.spec.ts
@@ -12,122 +12,139 @@ import { I18NRouterLoader, I18NRouterStaticLoader, I18NRouterHttpLoader, I18NRou
 import { testRoutes, testTranslations, testModuleConfig } from './index.spec';
 
 const mockBackendResponse = (connection: MockConnection, response: any) => {
-    connection.mockRespond(new Response(new ResponseOptions({ body: response })));
+  connection.mockRespond(new Response(new ResponseOptions({body: response})));
 };
 
 const mockBackendError = (connection: MockConnection, error: string) => {
-    connection.mockError(new Error(error));
+  connection.mockError(new Error(error));
 };
 
 describe('@nglibs/i18n-router:',
-    () => {
-        beforeEach(() => {
+  () => {
+    beforeEach(() => {
+      const i18nRouterFactory = () => new I18NRouterStaticLoader(testRoutes, testTranslations);
+
+      testModuleConfig(testRoutes, [{
+        provide: I18NRouterLoader,
+        useFactory: (i18nRouterFactory)
+      }]);
+    });
+
+    describe('I18NRouterLoader',
+      () => {
+        it('should not return any routes & translations unless provided',
+          () => {
+            const loader = new I18NRouterStaticLoader();
+            const loadedRoutes = loader.getRoutes();
+            const loadedTranslations = loader.getTranslations();
+
+            expect(loadedRoutes).toEqual([]);
+            expect(loadedTranslations).toBeUndefined();
+          });
+
+        it('should be able to provide `I18NRouterStaticLoader`',
+          () => {
             const i18nRouterFactory = () => new I18NRouterStaticLoader(testRoutes, testTranslations);
 
-            testModuleConfig(testRoutes, [{ provide: I18NRouterLoader, useFactory: (i18nRouterFactory) }]);
+            testModuleConfig(testRoutes, [{
+              provide: I18NRouterLoader,
+              useFactory: (i18nRouterFactory)
+            }]);
+
+            const injector = getTestBed();
+            const i18nRouter = injector.get(I18NRouterService);
+
+            expect(I18NRouterStaticLoader).toBeDefined();
+            expect(i18nRouter.loader).toBeDefined();
+            expect(i18nRouter.loader instanceof I18NRouterStaticLoader).toBeTruthy();
+          });
+
+        it('should be able to provide `I18NRouterHttpLoader`',
+          () => {
+            const i18nRouterFactory = (http: Http) => new I18NRouterHttpLoader(http);
+
+            testModuleConfig(testRoutes, [{
+              provide: I18NRouterLoader,
+              useFactory: (i18nRouterFactory),
+              deps: [Http]
+            }]);
+
+            const injector = getTestBed();
+            const i18nRouter = injector.get(I18NRouterService);
+
+            expect(I18NRouterHttpLoader).toBeDefined();
+            expect(i18nRouter.loader).toBeDefined();
+            expect(i18nRouter.loader instanceof I18NRouterHttpLoader).toBeTruthy();
+          });
+
+        it('should be able to provide any `I18NRouterLoader`',
+          () => {
+            class CustomLoader implements I18NRouterLoader {
+              loadTranslations(): any {
+                return Promise.resolve({});
+              }
+
+              getRoutes(): Routes {
+                return _.map(testRoutes, _.cloneDeep);
+              }
+
+              getTranslations(): any {
+                return testTranslations;
+              }
+            }
+
+            testModuleConfig(testRoutes, [{
+              provide: I18NRouterLoader,
+              useClass: CustomLoader
+            }]);
+
+            const injector = getTestBed();
+            const i18nRouter = injector.get(I18NRouterService);
+
+            expect(CustomLoader).toBeDefined();
+            expect(i18nRouter.loader).toBeDefined();
+            expect(i18nRouter.loader instanceof CustomLoader).toBeTruthy();
+          });
+      });
+
+    describe('I18NRouterHttpLoader',
+      () => {
+        beforeEach(() => {
+          const i18nRouterFactory = (http: Http) => new I18NRouterHttpLoader(http, testRoutes, '/api/get-routes');
+
+          testModuleConfig(testRoutes, [{
+            provide: I18NRouterLoader,
+            useFactory: (i18nRouterFactory),
+            deps: [Http]
+          }]);
         });
 
-        describe('I18NRouterLoader',
-            () => {
-                it('should not return any routes & translations unless provided',
-                    () => {
-                        const loader = new I18NRouterStaticLoader();
-                        const loadedRoutes = loader.getRoutes();
-                        const loadedTranslations = loader.getTranslations();
+        it('should be able to retrieve route translations from the specified `path`',
+          async(inject([MockBackend, I18NRouterService],
+            (backend: MockBackend, i18nRouter: I18NRouterService) => {
+              // mock response
+              backend.connections.subscribe((c: MockConnection) => mockBackendResponse(c, testTranslations));
 
-                        expect(loadedRoutes).toEqual([]);
-                        expect(loadedTranslations).toBeUndefined();
-                    });
-
-                it('should be able to provide `I18NRouterStaticLoader`',
-                    () => {
-                        const i18nRouterFactory = () => new I18NRouterStaticLoader(testRoutes, testTranslations);
-
-                        testModuleConfig(testRoutes, [{ provide: I18NRouterLoader, useFactory: (i18nRouterFactory) }]);
-
-                        const injector = getTestBed();
-                        const i18nRouter = injector.get(I18NRouterService);
-
-                        expect(I18NRouterStaticLoader).toBeDefined();
-                        expect(i18nRouter.loader).toBeDefined();
-                        expect(i18nRouter.loader instanceof I18NRouterStaticLoader).toBeTruthy();
-                    });
-
-                it('should be able to provide `I18NRouterHttpLoader`',
-                    () => {
-                        const i18nRouterFactory = (http: Http) => new I18NRouterHttpLoader(http);
-
-                        testModuleConfig(testRoutes, [{ provide: I18NRouterLoader, useFactory: (i18nRouterFactory), deps: [Http] }]);
-
-                        const injector = getTestBed();
-                        const i18nRouter = injector.get(I18NRouterService);
-
-                        expect(I18NRouterHttpLoader).toBeDefined();
-                        expect(i18nRouter.loader).toBeDefined();
-                        expect(i18nRouter.loader instanceof I18NRouterHttpLoader).toBeTruthy();
-                    });
-
-                it('should be able to provide any `I18NRouterLoader`',
-                    () => {
-                        class CustomLoader implements I18NRouterLoader {
-                            loadTranslations(): any {
-                                return Promise.resolve({});
-                            }
-
-                            getRoutes(): Routes {
-                                return _.map(testRoutes, _.cloneDeep);
-                            }
-
-                            getTranslations(): any {
-                                return testTranslations;
-                            }
-                        }
-
-                        testModuleConfig(testRoutes, [{ provide: I18NRouterLoader, useClass: CustomLoader }]);
-
-                        const injector = getTestBed();
-                        const i18nRouter = injector.get(I18NRouterService);
-
-                        expect(CustomLoader).toBeDefined();
-                        expect(i18nRouter.loader).toBeDefined();
-                        expect(i18nRouter.loader instanceof CustomLoader).toBeTruthy();
-                    });
-            });
-
-        describe('I18NRouterHttpLoader',
-            () => {
-                beforeEach(() => {
-                    const i18nRouterFactory = (http: Http) => new I18NRouterHttpLoader(http, testRoutes, '/api/get-routes');
-
-                    testModuleConfig(testRoutes, [{ provide: I18NRouterLoader, useFactory: (i18nRouterFactory), deps: [Http] }]);
+              i18nRouter.loader.loadTranslations()
+                .then((res: any) => {
+                  expect(res).toEqual(testTranslations);
                 });
+            })));
 
-                it('should be able to retrieve route translations from the specified `path`',
-                    async(inject([MockBackend, I18NRouterService],
-                        (backend: MockBackend, i18nRouter: I18NRouterService) => {
-                            // mock response
-                            backend.connections.subscribe((c: MockConnection) => mockBackendResponse(c, testTranslations));
+        it('should throw w/o a valid `path`',
+          async(inject([MockBackend, I18NRouterService],
+            (backend: MockBackend, i18nRouter: I18NRouterService) => {
+              // mock error
+              backend.connections.subscribe((c: MockConnection) => mockBackendError(c, '500'));
 
-                            i18nRouter.loader.loadTranslations()
-                                .then((res: any) => {
-                                    expect(res).toEqual(testTranslations);
-                                });
-                        })));
+              // this will produce error at the backend
+              i18nRouter.loader.loadTranslations()
+                .catch((res: any) => {
+                  expect(res).toEqual('Endpoint unreachable!');
 
-                it('should throw w/o a valid `path`',
-                    async(inject([MockBackend, I18NRouterService],
-                        (backend: MockBackend, i18nRouter: I18NRouterService) => {
-                            // mock error
-                            backend.connections.subscribe((c: MockConnection) => mockBackendError(c, '500'));
-
-                            // this will produce error at the backend
-                            i18nRouter.loader.loadTranslations()
-                                .catch((res: any) => {
-                                    expect(res).toEqual('Endpoint unreachable!');
-
-                                    const loadedTranslations = i18nRouter.loader.getTranslations();
-                                    expect(loadedTranslations).toBeUndefined();
-                                });
-                        })));
-            });
-    });
+                  const loadedTranslations = i18nRouter.loader.getTranslations();
+                  expect(loadedTranslations).toBeUndefined();
+                });
+            })));
+      });
+  });
