32a1dcf486fcb3b38b4109522a94899251ca3e20
Burak Tasci <burak.tasci@cozumtech.net>
2017-03-14 02:21:03 +0300

    style: lint

--- src/i18n-router.loader.ts
+++ src/i18n-router.loader.ts
@@ -8,48 +8,52 @@ import 'rxjs/add/operator/toPromise';
 import * as _ from 'lodash';
 
 export abstract class I18NRouterLoader {
-    abstract loadTranslations(): any;
-    abstract getRoutes(): Routes;
-    abstract getTranslations(): any;
+  abstract loadTranslations(): any;
+
+  abstract getRoutes(): Routes;
+
+  abstract getTranslations(): any;
 }
 
 export class I18NRouterStaticLoader implements I18NRouterLoader {
-    constructor(private readonly routes?: Routes,
-                private readonly translations?: any) {}
+  constructor(private readonly routes?: Routes,
+              private readonly translations?: any) {
+  }
 
-    loadTranslations(): any {
-        return Promise.resolve(this.translations);
-    }
+  loadTranslations(): any {
+    return Promise.resolve(this.translations);
+  }
 
-    getRoutes(): Routes {
-        return _.map(this.routes, _.cloneDeep);
-    }
+  getRoutes(): Routes {
+    return _.map(this.routes, _.cloneDeep);
+  }
 
-    getTranslations(): any {
-        return this.translations;
-    }
+  getTranslations(): any {
+    return this.translations;
+  }
 }
 
 export class I18NRouterHttpLoader implements I18NRouterLoader {
-    private translations: any;
+  private translations: any;
 
-    constructor(private readonly http: Http,
-                private readonly routes?: Routes,
-                private readonly path: string = '/routes.json') {}
+  constructor(private readonly http: Http,
+              private readonly routes?: Routes,
+              private readonly path: string = '/routes.json') {
+  }
 
-    loadTranslations(): any {
-        return this.http.get(this.path)
-            .map((res: any) => res.json())
-            .toPromise()
-            .then((translations: any) => this.translations = translations)
-            .catch(() => Promise.reject('Endpoint unreachable!'));
-    }
+  loadTranslations(): any {
+    return this.http.get(this.path)
+      .map((res: any) => res.json())
+      .toPromise()
+      .then((translations: any) => this.translations = translations)
+      .catch(() => Promise.reject('Endpoint unreachable!'));
+  }
 
-    getRoutes(): Routes {
-        return _.map(this.routes, _.cloneDeep);
-    }
+  getRoutes(): Routes {
+    return _.map(this.routes, _.cloneDeep);
+  }
 
-    getTranslations(): any {
-        return this.translations;
-    }
+  getTranslations(): any {
+    return this.translations;
+  }
 }
