dfb66214032d1c663960ac57f4dbf121c31d6dbd
Burak Tasci <burak.tasci@cozumtech.net>
2017-08-31 04:06:55 +0300

    test(config-loader): add mockBackend

---
+++ packages/@ngx-i18n-router/config-loader/testing/mocks/i18n-router-backend.mock.ts
@@ -0,0 +1,32 @@
+// angular
+import { BaseRequestOptions, Http, RequestMethod, Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+export const testSettings = {
+  routes: {
+    en: {
+      'ROOT.ABOUT': 'about'
+    },
+    tr: {
+      'ROOT.ABOUT': 'hakkinda'
+    }
+  }
+};
+
+export function fakeBackendFactory(backend: MockBackend, options: BaseRequestOptions): Http {
+  backend.connections
+    .subscribe((connection: MockConnection) => {
+      if (connection.request.url.endsWith('/config.json')
+        && connection.request.method === RequestMethod.Get)
+          connection.mockRespond(new Response(
+            new ResponseOptions({
+              status: 200,
+              body: testSettings
+            })
+          ));
+      else
+        connection.mockError(new Error('500'));
+    });
+
+  return new Http(backend, options);
+}
